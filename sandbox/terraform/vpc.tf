288ab79ea583af38afd44dde58f941a2c3d325f3
sreedharsasi98 <sreedharsasi102@gmail.com>
2025-02-26 15:45:03 +0530

    customize demo

---
+++ sandbox/terraform/vpc.tf
@@ -0,0 +1,44 @@
+# provider "aws" {
+#   region = "us-west-1"
+# }
+
+# Fetch available availability zones dynamically
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "dharani-vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
+resource "aws_vpc" "demo" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "demos-vpc"
+  }
+}
